1749b946b66e6186258d85434e9859bbe5c12745
JPZU <juanpablozp@icloud.com>
2024-02-04 14:07:51 -0500

    add the params for the website

--- my_app/views.py
+++ my_app/views.py
@@ -1,10 +1,21 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello Word</h1>")
+def index(request):
+    return HttpResponse('<h1>Index Page</h1>')
+
+def hello(request, username):
+    return HttpResponse("<h1>Hello %s</h1>"% username)
     
 def about(request):
     return HttpResponse('About')
     
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def task(request, id):
+    task = Task.objects.get(id=id)
+    return HttpResponse('Task: %s' % task.title)
